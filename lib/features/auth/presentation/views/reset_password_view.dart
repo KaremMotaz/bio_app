c080110d88cbac75fc514dac5f9eb665180ca179
Karim Motaz <karemmotaz2929@gmail.com>
2025-09-01 10:51:34 +0300

    Edit ui for reset password view

--- lib/features/auth/presentation/views/reset_password_view.dart
+++ lib/features/auth/presentation/views/reset_password_view.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../../../core/services/get_it_service.dart';
-import '../../../../core/theming/app_colors.dart';
-import '../../../../core/theming/text_styles.dart';
 import '../../../../core/widgets/custom_app_bar_pop_icon.dart';
 import '../../domain/auth_repo.dart';
 import '../manager/reset_password_cubit/reset_password_cubit.dart';
@@ -19,12 +16,7 @@ class ResetPasswordView extends StatelessWidget {
           ResetPasswordCubit(authRepo: getIt.get<AuthRepo>()),
       child: Scaffold(
         appBar: AppBar(
-          title: Text(
-            "نسيت كلمة السر",
-            style: TextStyles.bold18.copyWith(
-              color: AppColors.mainBlue,
-            ),
-          ),
+          title: const Text("نسيت كلمة السر"),
           leading: Navigator.of(context).canPop()
               ? const CustomAppBarPopIcon()
               : null,
